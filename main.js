0fad37197ec57c8532628efbaafb6636ff1b33b3
Willian Oliveira <willian.oliveira@visto.global>
2021-09-01 20:12:05 -0300

    Insere js

---
+++ main.js
@@ -0,0 +1,6 @@
+const proximo = document.querySelector(".proximo");
+const anterior = document.querySelector(".anterior");
+
+anterior.addEventListener('click', () => {
+    console.log("clicado");
+});
